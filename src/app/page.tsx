1f64460ca82810b302e48444d5a3c0ac8e54ea36
LazarPopov <lazarspopov@gmail.com>
2026-04-14 16:11:51 +0200

    ahrefs feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-// // src/app/page.tsx
-// import { permanentRedirect } from "next/navigation";
-
-// export default function RootPage() {
-//   // Use permanentRedirect to send a 308 status code
-//   permanentRedirect("/en");
-// }
+// src/app/page.tsx
+export default function RootPage() {
+  return null;
+}
